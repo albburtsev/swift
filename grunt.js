13e5da0c194f6b686e4af98038937bc3ef12470f
Alexander Burtsev <albburtsev@gmail.com>
2012-05-28 20:12:58 +0400

    Add gruntfile, add license, add default wrapper for js files

---
+++ grunt.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: '<json:package.json>',
+		meta: {
+			banner: '/*! Swift Map v@<%= pkg.version %> */'
+		},
+		concat: {
+			source: {
+				src: [
+					'js/src/_header.js',
+					'js/src/class.map.js',
+					'js/src/_footer.js',
+				],
+				dest: 'js/swift.js',
+				separator: '\n'
+			},
+			withbanner: {
+				src: ['<banner:meta.banner>', 'js/swift.js'],
+				dest: 'js/swift.js',
+				separator: '\n'
+			}
+		},
+		min: {
+			dist: {
+				src: ['<banner:meta.banner>', 'js/swift.js'],
+				dest: 'js/swift.min.js'
+			}
+		},
+		lint: {
+			files: ['js/swift.js']
+		},
+		jshint: {
+			options: {
+				smarttabs: true
+			}
+		},
+		watch: {
+			files: ['<config:concat.source.src>'],
+			tasks: 'concat'
+		}
+	});
+
+	grunt.registerTask('default', 'concat min lint watch');
+};
---
+++ js/swift.js
@@ -0,0 +1,12 @@
+/*! Swift Map v@1.0.0 */
+
+(function(window, document, undefined) {
+	
+	/**
+	 * @constructor
+	 */
+	function Map() {
+		
+	}
+
+})(window, document);
